cd7b6f5872900db8a06e6c7bac4bb0bd8b0dc1d7
Your Name <you@example.com>
2024-06-12 17:11:23 +0530

    Final Code in Pizza Order

--- P04_Pizza_Order.py
+++ P04_Pizza_Order.py
@@ -5,21 +5,26 @@ print("Thank you for choosing Python Pizza Deliveries!")
 
 # Adding price for the size
 size = input() 
-if (size.lower == "s"):
+if (size.lower() == "s"):
     final_bill += 15
-elif (size.lower == "m"):
+elif (size.lower() == "m"):
     final_bill += 20
 else:
     final_bill += 25
 
 # Adding price for the pepperoni
 add_pepperoni = input() 
-if (add_pepperoni.lower == "y"):
+if (add_pepperoni.lower() == "y" and size == "s"):
     final_bill += 2
+elif (add_pepperoni.lower() == "y" and size == "m" or size == "l"):
+    final_bill += 3
+else:
+    pass
 
 # Adding price for extra cheese
 extra_cheese = input()
-if (extra_cheese.lower == "y"):
+if (extra_cheese.lower() == "y"):
     final_bill += 1
 
+# Printing toatal bill
 print(f"Your final bill is: ${final_bill}.")
