656b4a9b5ad96e3348d9d25ee976b6d4f8f14552
Your Name <you@example.com>
2024-06-12 11:21:55 +0530

    Updates te BMI cal

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,6 +1,12 @@
-height = int(input("Enter your height in (kg) : "))
-weight = int(input("Enter your weight in (m) : "))
+# Getting Inputs
+height = float(input("Enter your height in (kg) : "))
+weight = float(input("Enter your weight in (m) : "))
 
+# Calculating BMI
 BMI = weight / (height * height)
 
+# Round BMI for two decimals
+round(BMI , 2)
+
+# Giving Output
 print(f"Your BMI is {BMI} ")
