7806335ec00675db8bc95f6aa7a103a3e5e983be
Your Name <you@example.com>
2024-06-12 15:57:24 +0530

    Correct condition

--- P03_Leap_Year.py
+++ P03_Leap_Year.py
@@ -3,8 +3,8 @@ year = int(input("Enter the year you want to convert : "))
 
 # Check the year is a leap year or not a leap year
 if (year % 4 == 0):
-    if (year % 4 == 0):
-        if (year % 4 == 0):
+    if (year % 100 == 0):
+        if (year % 400 == 0):
             print("Leap year")
         else:
             print("Not leap year")
