acf805abf684aa449730fac6d1c176994b400dc4
Your Name <you@example.com>
2024-06-17 17:00:10 +0530

    Comments in Challenge 05

--- P10_Turtle_Chllenge/Challenge_05.py
+++ P10_Turtle_Chllenge/Challenge_05.py
@@ -2,10 +2,10 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 
-for i in range (36):
-    tim.circle(100)
+for i in range (36):    # 36 circles 10 degree lef in all time so 360/10 = 36 
+    tim.circle(100)     # circle radius 100 
     tim.left(10)
-    tim.speed(12)
+    tim.speed(10)
 
 screen = Screen()
 screen.exitonclick()
