ab11acad4253b02bdaa1e3bc6c86f68c0c54526b
Your Name <you@example.com>
2024-06-17 10:31:49 +0530

    Create the screen

---
+++ P10_Turtle_Art.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+
+timmmy_the_turtle = Turtle()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()   # To display the screen
+screen.exitonclick()    # To exit the screen on a click
