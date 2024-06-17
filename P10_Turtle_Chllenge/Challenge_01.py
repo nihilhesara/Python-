d3606fe3958290c514f5e5737a3fc76c594e4575
Your Name <you@example.com>
2024-06-17 10:57:03 +0530

    create the exact code in for loop

--- P10_Turtle_Chllenge/Challenge_01.py
+++ P10_Turtle_Chllenge/Challenge_01.py
@@ -2,16 +2,9 @@ from turtle import Turtle, Screen
 
 timmmy_the_turtle = Turtle()    # Give a variable 
 
-timmmy_the_turtle.forward(100)
-
-timmmy_the_turtle.left(90)
-timmmy_the_turtle.forward(100)
-
-timmmy_the_turtle.left(90)
-timmmy_the_turtle.forward(100)
-
-timmmy_the_turtle.left(90)
-timmmy_the_turtle.forward(100)
+for i in range(4):
+    timmmy_the_turtle.forward(100)
+    timmmy_the_turtle.left(90)      # 90 degree turn left
 
 screen = Screen()   # To display the screen
 screen.exitonclick()    # To exit the screen on a click
