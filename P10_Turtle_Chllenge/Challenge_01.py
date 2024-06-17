ae6f34112b53e7bcc6da3d32dc03d241d1ebdea6
Your Name <you@example.com>
2024-06-17 10:49:14 +0530

    Create a square

--- P10_Turtle_Chllenge/Challenge_01.py
+++ P10_Turtle_Chllenge/Challenge_01.py
@@ -1,10 +1,17 @@
 from turtle import Turtle, Screen
 
-timmmy_the_turtle = Turtle()
+timmmy_the_turtle = Turtle()    # Give a variable 
 
+timmmy_the_turtle.forward(100)
 
+timmmy_the_turtle.left(90)
+timmmy_the_turtle.forward(100)
 
+timmmy_the_turtle.left(90)
+timmmy_the_turtle.forward(100)
 
+timmmy_the_turtle.left(90)
+timmmy_the_turtle.forward(100)
 
 screen = Screen()   # To display the screen
 screen.exitonclick()    # To exit the screen on a click
