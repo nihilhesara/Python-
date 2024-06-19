334e879b502e187e916e867c2d43d51872f84543
Nihil Hesara <nihilhesa@gmail.com>
2024-06-19 20:23:01 +0530

    Turtle push forward

--- P10_Turtle_Chllenge/Challenge_08.py
+++ P10_Turtle_Chllenge/Challenge_08.py
@@ -1,4 +1,7 @@
 from turtle import Turtle, Screen
+import random
+
+is_race_on = False
 
 screen = Screen()
 screen.setup(width = 500, height = 400)   # setting screen (width, height)
@@ -9,11 +12,22 @@ print(user_bet)
 
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_position = [-100, -60, -20, 20, 60, 100]
+all_turtles = []
 
 for turtle_index in range(0,6):
-    tim = Turtle(shape = "turtle")      # Change the shape of the arrow to turtle
-    tim.penup()
-    tim.goto(x = -230, y = y_position[turtle_index])
-    tim.color(colors[turtle_index])
+    new_turtle = Turtle(shape = "turtle")      # Change the shape of the arrow to turtle
+    new_turtle.penup()
+    new_turtle.goto(x = -230, y = y_position[turtle_index])
+    new_turtle.color(colors[turtle_index])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        random_distance = random.randint(0,10)
+        turtle.forward(random_distance)
 
 screen.exitonclick()
