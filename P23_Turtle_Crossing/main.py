002767303174a8086544a1553eb6f82df3a488e6
Nihil Hesara <nihilhesa@gmail.com>
2024-07-30 18:27:01 +0530

    Create car in random colors

--- P23_Turtle_Crossing/main.py
+++ P23_Turtle_Crossing/main.py
@@ -9,6 +9,9 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
+
+
 screen.listen()
 screen.onkey(player.go_up,"Up")
 
@@ -16,3 +19,6 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
