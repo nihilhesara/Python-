bca4d4a19cd97825ce1e29dc8e3e669f2a0cf8c3
Nihil Hesara <nihilhesa@gmail.com>
2024-07-30 18:41:41 +0530

    Detect car collusion

--- P23_Turtle_Crossing/main.py
+++ P23_Turtle_Crossing/main.py
@@ -22,3 +22,15 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move_cars()
+
+    # Detect collusion with the car
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+    
+    # Deatect a successful crossing 
+    if player.is_at_finish_line():
+        player.go_to_start()
+        car_manager.level_up()
+
+screen.exitonclick()
