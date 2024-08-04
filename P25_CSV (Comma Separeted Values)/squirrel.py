40d43b47f781a7e3dbac845f15481851151145e5
Nihil Hesara <nihilhesa@gmail.com>
2024-08-04 23:21:39 +0530

    Ceate a csv file using the data

--- P25_CSV (Comma Separeted Values)/squirrel.py
+++ P25_CSV (Comma Separeted Values)/squirrel.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas 
 import os
 
 # Initialize counters for each color
@@ -10,12 +10,22 @@ Black = 0
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 # Read CSV data into a DataFrame
-data = pd.read_csv("Squirrel_Data.csv")
+data = pandas.read_csv("Squirrel_Data.csv")
 
 # Filter and count occurrences for each fur color
-Grey = len(data[data.Primary_Fur_Color == "Gray"])
+Grey = len(data[data.Primary_Fur_Color == "Gray"])          # Use len function to count thr squirrels
 Cinnamon = len(data[data.Primary_Fur_Color == "Cinnamon"])
 Black = len(data[data.Primary_Fur_Color == "Black"])
 
 # Print the result
 print(f"Grey: {Grey}, Black: {Black}, Cinnamon: {Cinnamon}")
+
+# Crate a dict using the counts
+data_dict = {
+    "Fur_Color" : ["Gray","Cinnamon","Black"],
+    "Count" : [Grey,Cinnamon,Black]
+}
+
+# Convert dict into a csv file 
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
