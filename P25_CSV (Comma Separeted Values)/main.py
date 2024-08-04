b21de175d808fe644f7dac6c7625f06f928375ba
Nihil Hesara <nihilhesa@gmail.com>
2024-08-04 18:02:48 +0530

    Install pandass library

--- P25_CSV (Comma Separeted Values)/main.py
+++ P25_CSV (Comma Separeted Values)/main.py
@@ -1,15 +1,25 @@
 import os
-import csv
+import pandas             # python -m pip install pandas
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))    # move file path to the woking folder
 
 '''with open ("weather_data.csv") as data_file:
     data = data_file.readlines()                        # readlines get each line in the csv file and turn it into a list
-    print(data)'''
+    print(data)
+
+import csv                # open cmd and type this code below
 
 with open ("weather_data.csv") as data_file:
     data = csv.reader(data_file)
     
+    temperatures = []
     # to print each item in the csv folder as a list we have to loop through each row and print it 
     for row in data:
-        print(row)
+        if row[1] != "temp":
+            temperatures.append(int(row[1]))
+    print(temperatures)'''
+
+data = pandas.read_csv("weather_data.csv")
+print(data)
+print("\n\n")
+print(data["temp"])
