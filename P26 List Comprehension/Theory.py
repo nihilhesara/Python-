71dbe55b4ceb9a1ab069fdc6ad6c9a0673ec9acc
Nihil Hesara <nihilhesa@gmail.com>
2024-08-12 22:40:38 +0530

    List comprehension theory

---
+++ P26 List Comprehension/Theory.py
@@ -0,0 +1,16 @@
+# Nomal List
+
+numbers = [1,2,3]
+new_list = []
+
+for n in numbers:
+    add_1 = n + 1
+    new_list.append(add_1)
+
+print(new_list)
+
+# List comprehension
+
+numbers = [1,2,3]
+new_list = [n+1 for n in numbers]
+print(new_list)
