b5d7cdf75471085081ae7a90179f989e6361d93d
Nihil Hesara <nihilhesa@gmail.com>
2024-08-13 13:14:39 +0530

    alphabet game

--- NATO alphabet/main.py
+++ NATO alphabet/main.py
@@ -1,4 +1,4 @@
-student_dict = {
+'''student_dict = {
     "student": ["Angela", "James", "Lily"], 
     "score": [56, 76, 98]
 }
@@ -18,10 +18,18 @@ for (index, row) in student_data_frame.iterrows():
     pass
 
 # Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+# {new_key:new_value for (index, row) in df.iterrows()}'''
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+import pandas
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter: row.code for (index,row) in data.iterrows()}
+
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
+word = input("Enter a word : ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
