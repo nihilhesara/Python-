1e8d367408775ca2404383617d06500c5ae06743
Nihil Hesara <nihilhesa@gmail.com>
2024-08-14 13:50:25 +0530

    kwargs

--- P27_Tkinter/playground.py
+++ P27_Tkinter/playground.py
@@ -8,3 +8,15 @@ def add(*args): # put * to say there are multiple values and args is a variable 
 print(add(1,2,3,4))
 print(add(1,2,3,4,5,6,7))
 print(add(1,2,3,4,5,6,7,8,9,10))
+
+# ___________________________________________________________________________________________________________________
+
+# keyword argument (kwargs)
+def calculate(n, **kwargs):
+    print(kwargs)           # It keeps the values that pass the user as a dictionay 
+
+    n += kwargs["add"]
+    n *= kwargs["multiply"] 
+    print(n)
+
+calculate(2, add=3, multiply=5)
