cdc7c3abf02de5228f9e11ecd844bf73cf7d8aa2
Nihil Hesara <nihilhesa@gmail.com>
2024-08-14 15:54:53 +0530

    TK inter basics

--- P27_Tkinter/main.py
+++ P27_Tkinter/main.py
@@ -1,9 +1,32 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My first GUI program")    # Window title
-window.minsize(width=500,height=300)    # Minimum size of the window 
-my_label = tkinter.Label(text="I am a label",font=("Arial",24,"bold"))   # Create the label (It doesn't display)
-my_label.pack(side="left")                                 # Setting the label in the window (Default side is center)
+window.minsize(width=500, height=300)    # Minimum size of the window
 
-window.mainloop()       # Keep the screen 
+# Create the label (It displays now)
+my_label = Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label.pack() # my_label.pack(side="left")  # Setting the label in the window (Default side is center)
+
+my_label["text"] = "New text"  # Modify the text of the label using dictionary-like indexing
+my_label.config(text="New Text")  # Another way to modify the label text using the config method
+
+def button_clicked():
+    print("I got clicked")
+    my_label.config(text="Button got clicked")
+    new_text = input.get()
+    my_label.config(text= new_text)
+
+# Button
+button = Button(text="Click me", command=button_clicked) # we put name of the function in command attribute 
+button.pack()
+
+# Entry (create a text box)
+input = Entry(width=10)
+input.pack()
+print(input.get())
+
+# ______________________________________________________________________________________________________________________
+
+
+window.mainloop()  # Keep the screen
