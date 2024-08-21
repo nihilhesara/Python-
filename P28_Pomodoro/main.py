e2ef4d62ea9952006a8381e12bc0289221886bf9
Nihil Hesara <nihilhesa@gmail.com>
2024-08-21 12:46:41 +0530

    create labels and buttons

--- P28_Pomodoro/main.py
+++ P28_Pomodoro/main.py
@@ -21,10 +21,21 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+timer = Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
+timer.grid(column=1,row=0)
+
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file=image_path)
 canvas.create_image(100, 112, image=tomato_img)
-canvas.pack()
+canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME,35,"bold"))
+canvas.grid(column=1, row=1)
 
 window.mainloop()
