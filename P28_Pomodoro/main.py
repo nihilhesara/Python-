bcea7fa1c78af16a8139ada983bdf6bc5c6aa7ab
Nihil Hesara <nihilhesa@gmail.com>
2024-08-21 13:22:05 +0530

    UI of pomodoro

--- P28_Pomodoro/main.py
+++ P28_Pomodoro/main.py
@@ -23,19 +23,22 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-timer = Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
-timer.grid(column=1,row=0)
+title = Label(text="Timer", font=(FONT_NAME, 35, "bold"), fg=GREEN, bg=YELLOW)
+title.grid(column=1,row=0)
 
-start_button = Button(text="Start")
-start_button.grid(column=0, row=2)
-
-reset_button = Button(text="Reset")
-reset_button.grid(column=2, row=2)
-
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0) # highlightthickness=0 get rid of the border
 tomato_img = PhotoImage(file=image_path)
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME,35,"bold"))
 canvas.grid(column=1, row=1)
+
+start_button = Button(text="Start",highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset",highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_mark = Label(text="✔",fg=GREEN, bg=YELLOW)
+check_mark.grid(column=1,row=3)
 
 window.mainloop()
