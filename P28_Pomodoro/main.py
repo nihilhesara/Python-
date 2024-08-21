bcca6189e50cb37d10bbf37f37c73b8a7671da93
Nihil Hesara <nihilhesa@gmail.com>
2024-08-21 20:48:58 +0530

    Countdown machanisum

--- P28_Pomodoro/main.py
+++ P28_Pomodoro/main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import os
+import os 
 image_path = os.path.join("P28_Pomodoro", "tomato.png")
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -16,7 +16,34 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    global reps
+    count_down(5 * 60)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+"""
+----------------- AFTER METHOD ---------------------- 
+def say_something(thing):
+    print(thing)
+
+window.after(1000,say_something,"hello")
+# (waiting time in miliseconds, funciton name , value or values that you pass to the function )"""
+
+def count_down(count):
+
+    count_min = count // 60
+    count_sec = count % 60
+
+    if count_sec == 0:
+        count_sec = "00"
+    
+    elif count_sec <= 9:
+        count_sec = f"0{count_sec}"
+
+    canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000,count_down,count -1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -29,10 +56,10 @@ title.grid(column=1,row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0) # highlightthickness=0 get rid of the border
 tomato_img = PhotoImage(file=image_path)
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME,35,"bold"))
+timer_text = canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME,35,"bold"))
 canvas.grid(column=1, row=1)
 
-start_button = Button(text="Start",highlightthickness=0)
+start_button = Button(text="Start",highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset",highlightthickness=0)
---
+++ Theory/T04_Dynamic_Typing.py
@@ -0,0 +1,10 @@
+# 28
+
+a = 3
+print(type(a))
+a = "Hello"
+print(type(a))
+
+# You can work with two data types at once if the user doesn't need a specific data type
+# This is not in all programming languages C , Java , Swift can't do this 
+# further - https://stackoverflow.com/questions/11328920/is-python-strongly-typed
