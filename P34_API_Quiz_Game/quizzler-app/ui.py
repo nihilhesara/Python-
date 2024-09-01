06b7d01fdf341e897ea1955c10bad3cbc93f356d
Nihil Hesara <nihilhesa@gmail.com>
2024-09-01 23:40:58 +0530

    Create the UI

--- P34_API_Quiz_Game/quizzler-app/ui.py
+++ P34_API_Quiz_Game/quizzler-app/ui.py
@@ -1,4 +1,7 @@
 from tkinter import *
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 THEME_COLOR = "#375362"
 
@@ -7,5 +10,21 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzer")
+        self.window.config(padx=20,pady=20, bg=THEME_COLOR)
+
+        self.score = Label(text="Score: 0", font=("Arial", 15, "italic"), fg="white", bg=THEME_COLOR)
+        self.score.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(150,125,text="Some Question text", fill=THEME_COLOR, font=("Arial", 20, "italic"))
+        self.canvas.grid(row=1,column=0,columnspan=2,pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2, column=1)
 
         self.window.mainloop()
