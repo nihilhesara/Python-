0364b29bfbe647ce0a51881a93ea3a45bbff6f0a
Nihil Hesara <nihilhesa@gmail.com>
2024-09-01 21:30:33 +0530

    ceate API

--- P34_API_Quiz_Game/quizzler-app/data.py
+++ P34_API_Quiz_Game/quizzler-app/data.py
@@ -1,3 +1,15 @@
+import requests
+
+parameters = {
+    "amount":10,
+    "type":"boolean"
+}
+
+response = requests.get("https://opentdb.com/api.php", params=parameters)
+response.raise_for_status()
+data = response.json()
+print(data["results"])
+
 question_data = [
     {
         "category": "Science: Computers",
