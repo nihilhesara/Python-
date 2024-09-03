e01bc180825ea9b3db3b1aaad6d730d0db69bb5a
Nihil Hesara <nihilhesa@gmail.com>
2024-09-03 14:37:19 +0530

    type hints theory

--- Theory/T06_Pyton_cloud.py
+++ Theory/T06_Pyton_cloud.py
@@ -1,2 +1,13 @@
 # https://www.pythonanywhere.com/
 # day - 32
+
+# day - 34 (Type hints)
+
+def police_check(age:int) -> bool: # (variable name: data type) -> output data type IDE gives error when the type doesn't match
+    if age > 18:
+        can_drive = True
+    else:
+        can_drive = False
+    return can_drive
+
+print(police_check(12))
