f647a9759bf1782b43d9609615af3c64b8e03b82
Nihil Hesara <nihilhesa@gmail.com>
2024-09-11 16:47:39 +0530

    Check if it rain and print the message in the console

--- P35_API_Authentication_Keys/main.py
+++ P35_API_Authentication_Keys/main.py
@@ -3,12 +3,24 @@ import requests
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
 api_key = "58f83b5a48ee141fb8a923235d729c17"
 # https://api.openweathermap.org/data/2.5/weather?q=Colombo&appid=58f83b5a48ee141fb8a923235d729c17
+# https://api.openweathermap.org/data/2.5/forecast?lat=6.9319&lon=79.8478&appid=58f83b5a48ee141fb8a923235d729c17
 
 weather_param = {
     "lat":6.9319,
     "lon":79.8478,
     "appid": api_key,
+    "cnt":4,
 }
 
 response = requests.get(OWM_ENDPOINT,params=weather_param)
-print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella.")
