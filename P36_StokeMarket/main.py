e214aa56197c6da299e59b380ddd78be343c168d
Nihil Hesara <nihilhesa@gmail.com>
2024-09-18 01:05:01 +0530

    First HTML

--- P36_StokeMarket/main.py
+++ P36_StokeMarket/main.py
@@ -4,6 +4,8 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
+STOKE_API_KEY = "TTUP9JHDJJPATPE7"
+
     ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
 
